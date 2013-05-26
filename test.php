ba5433be0be44ada6c95c5cd587d7bf6e7bdabfc
Roman Hromenkov <roman.hromenkov@gmail.com>
2013-05-26 22:53:26 +0300

    First commit. Adding a Readme.

---
+++ test.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bonik test</title>
+</head>
+<body>
+	<h1><?php echo "Bonik PHP tutorial"; ?></h1>
+	<p>&copy <?php echo date('Y'); ?> Bonik php</p>
+</body>
+</html>
